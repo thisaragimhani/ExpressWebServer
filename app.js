ecfb07bb9dbd3b06fce63e53da47fabeace27022
thisaragimhani <thisara.gim@gmail.com>
2021-02-24 08:41:36 +0530

    server changes1- ejs used as view engine

--- app.js
+++ app.js
@@ -1,41 +1,57 @@
 const express = require('express')//pull in express package
 const app = express() //create new instance of expres so we can actually use that - can give any variable name
-//const path = require("path") 
+const path = require('path') 
 const port = 3000
 
 app.use(express.urlencoded({extended:false})) 
-//app.use(getWeather)
+app.use(getWeather)
 //app.use(express.static(path.join(_dirname, "public")))
 
-//app.set("view engine", "ejs")
-//app.set("views", path.join(_dirname, "view"))
+app.set("view engine", "ejs")
+app.set("views", path.join(__dirname, "view"))
 
 //set up route with two arguments - url , arrow function
-app.get('/', (req, res) => {
-  res.send(`
-  <h1>What color is the sky on a clear day?</h1>
-  <form action="/result" method="POST">
-    <input type="text" name="color">
-    <button>SUBMIT ANSWER</button>
-  </form>
-  `) //send back a message
+function getWeather(req, res, next) {
+  req.visitorWeather = false
+  if (req.visitorWeather) {
+    res.send("Please come back to our app when it is not raining.")
+  } else {
+    next()
+  }
+}
+
+app.get("/", (req, res) => {
+  res.render("home", {
+    isRaining: req.visitorWeather,
+    pets: [
+      { name: "Meowsalot", species: "cat" },
+      { name: "Barksalot", species: "dog" }
+    ]
+  })
 })
 
-app.get('/about', (req, res) => {
-    res.send('user Hello World!') //send back a message
-  })
+app.get("/about", (req, res) => {
+  res.send("Thanks for learning more about us.")
+})
 
-app.post("/result", (req,res)=>{
-    if(req.body.color.trim().toUpperCase() === "BLUE"){
-        res.send("Congrats, that is correct. ") 
-    }else {
-        res.send ("Incorrect, please try again.")
-    } 
-})  
+app.post("/result", (req, res) => {
+  if (req.body.color.trim().toUpperCase() === "BLUE") {
+    res.send("Congrats, that is correct.")
+  } else {
+    res.send("Incorrect, please try again.")
+  }
+})
 
-app.get('/result', (req, res) => {
-    res.send('Why are you here!') //send back a message
-  })
+app.get("/result", (req, res) => {
+  res.send("Why are you visiting this?")
+})
+
+app.get("/api/pets", (req, res) => {
+  res.json([
+    { name: "Meowsalot", species: "cat" },
+    { name: "Barksalot", species: "dog" }
+  ])
+})
   
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
